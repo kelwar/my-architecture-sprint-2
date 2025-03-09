5cb25d91c130d0e7e601c355f3f0bcf0696d0d02
Artem Ufimtsev <kelwar@elbrus.ru>
2025-03-10 00:25:54 +0300

    Исправлены порты и команды запуска реплик обоих шардов в compose.yml директорий mongo-sharding-repl и sharding-repl-cache, заменены порты реплик в командах инициализации обоих шардов, реализовано добавление всех реплик обоих шардов в инициализацию роутера

--- mongo-sharding-repl/scripts/shard1-init.sh
+++ mongo-sharding-repl/scripts/shard1-init.sh
@@ -10,8 +10,8 @@ rs.initiate(
       _id : "shard1",
       members: [
         { _id : 0, host : "shard1_1:27018" },
-        { _id : 1, host : "shard1_2:27021" },
-        { _id : 2, host : "shard1_3:27022" }
+        { _id : 1, host : "shard1_2:27018" },
+        { _id : 2, host : "shard1_3:27018" }
       ]
     }
 )
--- mongo-sharding-repl/scripts/shard2-init.sh
+++ mongo-sharding-repl/scripts/shard2-init.sh
@@ -10,8 +10,8 @@ rs.initiate(
       _id : "shard2",
       members: [
         { _id : 0, host : "shard2_1:27019" },
-        { _id : 1, host : "shard2_2:27023" },
-        { _id : 2, host : "shard2_3:27024" }
+        { _id : 1, host : "shard2_2:27019" },
+        { _id : 2, host : "shard2_3:27019" }
       ]
     }
 )
